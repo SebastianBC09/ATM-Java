dc0d9ab24b225a162c493c9895a6fd62e97127ec
Sebastian Ballen <sebastian.ballenc@gmail.com>
2024-05-24 20:20:07 -0500

    feat: new abstract class created

---
+++ src/main/resources/user/User.java
@@ -0,0 +1,23 @@
+package main.resources.user;
+
+public abstract class User {
+		protected String userId;
+		protected String name;
+		protected String pin;
+		protected double balance;
+
+	public User(String userId, String name, String pin, double balance) {
+		this.userId = userId;
+		this.name = name;
+		this.pin = pin;
+		this.balance = balance;
+	}
+
+	public abstract void displayUserMenu();
+
+	public abstract void makeAWithdrawal(double amount);
+
+	public abstract void makeADeposit(double amount);
+
+	public abstract void changePin(String newPin);
+}
