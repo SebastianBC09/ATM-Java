902c956b40653ed4f3a9441eedf3d4099a0c5809
Sebastian Ballen <sebastian.ballenc@gmail.com>
2024-05-24 21:54:04 -0500

    feat: new method override to display user menu. Still few errors regarding options methods.

--- src/main/resources/user/PrimaryAccount.java
+++ src/main/resources/user/PrimaryAccount.java
@@ -1,5 +1,9 @@
 package main.resources.user;
 
+import javax.swing.JOptionPane;
+
+
+
 public class PrimaryAccount extends User{
 
 
@@ -9,11 +13,49 @@ public class PrimaryAccount extends User{
 
 	@Override
 	public void displayUserMenu() {
+		String menuOptions = """
+			Primary User Menu:
+			1. Check Balance
+			2. Withdraw
+			3. Deposit
+			4. Transfer to Joint Account
+			5. Change account pin
+			6. Exit""";
 
+		String menuChoice = JOptionPane.showInputDialog(null, menuOptions, "ATM Menu", JOptionPane.PLAIN_MESSAGE);
+
+		if (menuChoice != null) {
+			switch (menuChoice) {
+				case "1":
+					showAccountBalance();
+					break;
+				case "2":
+					makeAWithdrawal(double amount)
+					break;
+				case "3":
+					makeADeposit();
+					break;
+				case "4":
+					transferToJoint();
+					break;
+				case "5":
+					changePin();
+				case "6":
+					System.exit(0); // Exit the program
+				default:
+					JOptionPane.showMessageDialog(null, "Invalid menuChoice. Please try again.", "Error", JOptionPane.ERROR_MESSAGE);
+					displayUserMenu(); // Display the menu again
+			}
+		}
 	}
 
 	@Override
-	public void makeAWithdrawal(double amount) {
+	public void showAccountBalance() {
+	}
+
+	@Override
+	public boolean makeAWithdrawal(double amount) {
+
 	}
 
 	@Override
@@ -22,6 +64,9 @@ public class PrimaryAccount extends User{
 
 	@Override
 	public void changePin(String newPin) {
+	}
+
+	public void transferToJoint() {
 	}
 
 }
