55f59059d7ee1eea62c1df99bda6801e2ce668ef
Sebastian Ballen <sebastian.ballenc@gmail.com>
2024-05-24 22:35:30 -0500

    feat: new user method created, primary account type.

--- src/main/java/com/atm/AppATM.java
+++ src/main/java/com/atm/AppATM.java
@@ -1,7 +1,12 @@
 package main.java.com.atm;
 
+import main.resources.user.PrimaryAccount;
+
 public class AppATM {
 	public static void main(String[] args) {
+		PrimaryAccount account_01 = new PrimaryAccount("ASD0910", "Sebastian", "0603", 0.0);
+
+		account_01.displayUserMenu();
 
 	}
 }
