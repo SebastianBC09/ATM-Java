7400048b025248efc540e91070d34d745517e92d
Sebastian Ballen <sebastian.ballenc@gmail.com>
2024-05-24 22:26:12 -0500

    feat: joint account class errors fixed

--- src/main/resources/user/JointAccount.java
+++ src/main/resources/user/JointAccount.java
@@ -1,4 +1,10 @@
+//PACKAGE DECLARATION
 package main.resources.user;
+
+//IMPORTS
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import javax.swing.JOptionPane;
 
 public class JointAccount extends User{
 
@@ -12,18 +18,31 @@ public class JointAccount extends User{
 	}
 
 	@Override
-	public void makeAWithdrawal(double amount) {
+	public void showAccountBalance() {
+		LocalDateTime currentDateTime = LocalDateTime.now();
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+		String formattedDateTime = currentDateTime.format(formatter);
+
+		double balance = this.balance; // Access the balance attribute directly
+		String message = "Account Balance: $" + balance + "\nEnquiry Date and Time: " + formattedDateTime;
+
+		JOptionPane.showMessageDialog(null, message, "Account Balance", JOptionPane.INFORMATION_MESSAGE);
+	}
+
+	@Override
+	public void makeAWithdrawal() {
 
 	}
 
 	@Override
-	public void makeADeposit(double amount) {
-
+	public void makeADeposit() {
 	}
 
 	@Override
-	public void changePin(String newPin) {
+	public void changePin() {
+	}
 
+	public void transferToJoint() {
 	}
 
 }
