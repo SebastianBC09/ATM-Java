7fac7da241d46a8f8e9f1d1e2a222dc33d1d2006
Sebastian Ballen <sebastian.ballenc@gmail.com>
2024-05-28 20:47:13 -0500

    feat: new methods added to main & necessary imports added

--- src/main/java/com/atm/AppATM.java
+++ src/main/java/com/atm/AppATM.java
@@ -1,11 +1,21 @@
 package main.java.com.atm;
 
+import main.resources.user.User;
 import main.resources.user.PrimaryAccount;
+import main.resources.login.Login;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AppATM {
+
+	private static List<User> users = new ArrayList<>();
+
 	public static void main(String[] args) {
 		PrimaryAccount account_01 = new PrimaryAccount("ASD0910", "Sebastian", "0603", 0.0);
 
+		users.add(account_01);
+		Login.authentication(users);
 		account_01.displayUserMenu();
 
 	}
